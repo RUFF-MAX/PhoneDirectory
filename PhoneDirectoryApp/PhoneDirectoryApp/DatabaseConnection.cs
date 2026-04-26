c9695b7e6cb25721cf875cf6e58d6db558ef60c2
Максим <rudikmansimaks87@gmail.com>
2026-04-26 16:59:09 +0700

    Создан конфиг и класс singleton для поключения

---
+++ PhoneDirectoryApp/PhoneDirectoryApp/DatabaseConnection.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PhoneDirectoryApp
+{
+    /// <summary>
+    /// Представляет класс для управления подключением к базе данных.
+    /// Реализует паттерн Singleton для обеспечения одного экземпляра подключения.
+    /// </summary>
+    public class DatabaseConnection
+    {
+        private static DatabaseConnection _instance;
+        private SqlConnection _connection;
+
+        /// <summary>
+        /// Строка подключения, полученная из конфигурационного файла.
+        /// </summary>
+        private string _connectionString =
+        ConfigurationManager.ConnectionStrings["DirectoryConnection"]
+        .ConnectionString;
+
+        /// <summary>
+        /// Инициализирует подключение к базе данных.
+        /// </summary>
+        private DatabaseConnection()
+        {
+            _connection = new SqlConnection(_connectionString);
+        }
+
+        /// <summary>
+        /// Возвращает единственный экземпляр класса DatabaseConnection. Если экземпляр ещё не создан, создаёт его.
+        /// </summary>
+        /// <returns>Экземпляр DatabaseConnection.</returns>
+        public static DatabaseConnection GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new DatabaseConnection();
+            }
+            return _instance;
+        }
+
+        /// <summary>
+        /// Возвращает активное подключение к базе данных. Если подключение закрыто, оно будет открыто.
+        /// </summary>
+        /// <returns></returns>
+        public SqlConnection GetConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+            return _connection;
+        }
+
+        /// <summary>
+        /// Закрывает подключение к базе данных, если оно открыто.
+        /// </summary>
+        public void CloseConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
+
+        public bool TestConnection()
+        {
+            try
+            {
+                if (_connection.State != ConnectionState.Open)
+                    _connection.Open();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка подключения: " + ex.Message);
+                return false;
+            }
+            finally
+            {
+                if (_connection.State == ConnectionState.Open)
+                    _connection.Close();
+            }
+        }
+    }
+}
---
+++ PhoneDirectoryApp/PhoneDirectoryApp/MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace PhoneDirectoryApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справочникиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.контактыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.адресаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.организацииToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.группыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.номераТелефоновToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.помощьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справочникиToolStripMenuItem,
+            this.помощьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(884, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "DataBaseStrip";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // справочникиToolStripMenuItem
+            // 
+            this.справочникиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.контактыToolStripMenuItem,
+            this.адресаToolStripMenuItem,
+            this.организацииToolStripMenuItem,
+            this.группыToolStripMenuItem,
+            this.номераТелефоновToolStripMenuItem});
+            this.справочникиToolStripMenuItem.Name = "справочникиToolStripMenuItem";
+            this.справочникиToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.справочникиToolStripMenuItem.Text = "Справочники";
+            // 
+            // контактыToolStripMenuItem
+            // 
+            this.контактыToolStripMenuItem.Name = "контактыToolStripMenuItem";
+            this.контактыToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.контактыToolStripMenuItem.Text = "Контакты";
+            // 
+            // адресаToolStripMenuItem
+            // 
+            this.адресаToolStripMenuItem.Name = "адресаToolStripMenuItem";
+            this.адресаToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.адресаToolStripMenuItem.Text = "Адреса";
+            // 
+            // организацииToolStripMenuItem
+            // 
+            this.организацииToolStripMenuItem.Name = "организацииToolStripMenuItem";
+            this.организацииToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.организацииToolStripMenuItem.Text = "Организации";
+            // 
+            // группыToolStripMenuItem
+            // 
+            this.группыToolStripMenuItem.Name = "группыToolStripMenuItem";
+            this.группыToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.группыToolStripMenuItem.Text = "Группы";
+            // 
+            // номераТелефоновToolStripMenuItem
+            // 
+            this.номераТелефоновToolStripMenuItem.Name = "номераТелефоновToolStripMenuItem";
+            this.номераТелефоновToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.номераТелефоновToolStripMenuItem.Text = "Номера телефонов";
+            // 
+            // помощьToolStripMenuItem
+            // 
+            this.помощьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem});
+            this.помощьToolStripMenuItem.Name = "помощьToolStripMenuItem";
+            this.помощьToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.помощьToolStripMenuItem.Text = "Помощь";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(884, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(900, 600);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Телефонный справочник";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справочникиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem контактыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem адресаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem организацииToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem группыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem номераТелефоновToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem помощьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+    }
+}
+
